76ddf3193a5856e399ffec03c63c416fef63e0f1
sagar rajak <sagarrajak245@gmail.com>
2023-12-27 21:40:27 +0530

    add existing project files to github

---
+++ lib/models/Expense.dart
@@ -0,0 +1,60 @@
+import 'package:uuid/uuid.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
+
+const uuid = Uuid();
+
+enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.fastfood,
+  Category.travel: Icons.flight,
+  Category.leisure: Icons.beach_access,
+  Category.work: Icons.work,
+};
+
+class Expense {
+  Expense({
+    required this.name,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+
+  final String id;
+  final String name;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allexpenses, this.category)
+      : expenses = allexpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
+}
